47c4d1b675f55b7f52b8a45afe1e530233b53311
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2024-07-12 08:57:32 +0900

    ユーザー一覧画面作成

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,15 @@
+import {  Wrap, WrapItem } from '@chakra-ui/react';
 import { VFC, memo } from 'react';
+import { UserCard } from '../organisms/user/UserCard';
 
 export const UserManagement: VFC = memo(() => {
-  return <p>user</p>;
+  return (
+    <Wrap p={{base:4,md:10}}>
+    {[...Array(10)].map(()=>(
+      <WrapItem>
+        <UserCard imageUrl='https://picsum.photos/800' userName='あんぱん' fullName='annpann'></UserCard>
+      </WrapItem>
+      ))}
+    </Wrap>
+  )
 });
