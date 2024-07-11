dbd3e3d55e0923da1d7fde25ad8b4d580b44d18e
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2024-07-11 18:17:42 +0900

    Headerとハンバーガーメニューの作成

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -3,6 +3,7 @@ import { Login } from '../components/pages/Login';
 import { Routes, Route } from 'react-router-dom';
 import { homeRoutes } from './HomeRoutes';
 import { Page404 } from '../components/pages/Page404';
+import { HeaderLayout } from '../components/templates/HeaderLayout';
 
 export const Router: VFC = memo(() => {
   return (
@@ -18,7 +19,7 @@ const HomeRoutes: VFC = memo(() => {
   return (
     <Routes>
       {homeRoutes.map((route) => (
-        <Route key={route.path} path={route.path} element={route.children} />
+        <Route key={route.path} path={route.path} element={<HeaderLayout>{route.children}</HeaderLayout>} />
       ))}
     </Routes>
   );
