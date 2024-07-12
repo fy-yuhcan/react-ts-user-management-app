d9aace8f43fa3daed01b404bbf5adf6471f7528e
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2024-07-12 09:26:08 +0900

    ユーザー一覧取得機能

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,15 +1,34 @@
-import {  Wrap, WrapItem } from '@chakra-ui/react';
-import { VFC, memo } from 'react';
+import { Center, Spinner, Wrap, WrapItem } from '@chakra-ui/react';
+import { VFC, memo, useEffect } from 'react';
 import { UserCard } from '../organisms/user/UserCard';
+import { useAllUsers } from '../../hooks/useAllUsers';
 
 export const UserManagement: VFC = memo(() => {
+  const { getUsers, users, loading } = useAllUsers();
+
+  useEffect(() => {
+    getUsers();
+  }, [getUsers]);
+
   return (
-    <Wrap p={{base:4,md:10}}>
-    {[...Array(10)].map(()=>(
-      <WrapItem>
-        <UserCard imageUrl='https://picsum.photos/800' userName='あんぱん' fullName='annpann'></UserCard>
-      </WrapItem>
-      ))}
-    </Wrap>
-  )
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map((user) => (
+            <WrapItem key={user.id} mx="auto">
+              <UserCard 
+                imageUrl='https://picsum.photos/800' 
+                userName={user.username} 
+                fullName={user.name} 
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
+  );
 });
